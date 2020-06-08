da794c2cb7543b540b0b0a9dcf582462ce94033f
Romain <romainmostras@icloud.com>
2020-06-08 17:06:43 +0200

    modify user table

--- db/migrate/20200608144301_devise_create_users.rb
+++ db/migrate/20200608144301_devise_create_users.rb
@@ -6,6 +6,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :first_name
+      t.string :last_name
+      t.string :address
+      t.string :zip_code
+      t.string :city
+      t.string :phone_number
 
       ## Recoverable
       t.string   :reset_password_token
