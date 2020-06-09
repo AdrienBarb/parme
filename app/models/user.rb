34e5d8d6df009a939315e2e42179d7ead6dc2ecf
Romain <romainmostras@icloud.com>
2020-06-09 11:45:46 +0200

    changement regex dans model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :address, presence: true
-  validates :zip_code, presence: true, format: { with: /[0-9]{5}/ }
+  validates :zip_code, presence: true, format: { with: /\A[0-9]{5}\z/ }
   validates :city, presence: true
-  validates :phone_number, presence: true, uniqueness: true, format: { with: /\A0(6|7)\s([0-9]{2}\s){3}([0-9]{2})$/ }
+  validates :phone_number, presence: true, uniqueness: true, format: { with: /\A0(6|7)\s([0-9]{2}\s){3}([0-9]{2})\z/ }
 end
