db2ad40cfd036dbf46fd908459306c19be71fab7
Romain <romainmostras@icloud.com>
2020-06-09 15:05:39 +0200

    Changing dependant destroy in the 4 models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
-  belongs_to :dress, dependent: :destroy
+  belongs_to :dress
 
   validates :start_rent_at, presence: true
   validates :end_rent_at, presence: true
