8d0024fe42a23d155e469b33f2f6334bf7b9c519
Adrien Barbier <adrien-barbier@hotmail.fr>
2020-06-09 14:35:13 +0200

    Final update

--- app/controllers/dresses_controller.rb
+++ app/controllers/dresses_controller.rb
@@ -1,6 +1,6 @@
 class DressesController < ApplicationController
   def index
     all_dresses = Dress.joins(:user).where(users: params[:city])
-    @dresses = all_dresses.select{ |dress| dress.available }
+    @dresses #= all_dresses.select{ |dress| dress.available }
   end
 end
