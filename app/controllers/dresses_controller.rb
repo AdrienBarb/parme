2aa8ff17390dad64625295344354d40b49b13fa6
Adrien Barbier <adrien-barbier@hotmail.fr>
2020-06-09 15:47:16 +0200

    Creation show view

--- app/controllers/dresses_controller.rb
+++ app/controllers/dresses_controller.rb
@@ -1,6 +1,11 @@
 class DressesController < ApplicationController
   def index
-    all_dresses = Dress.joins(:user).where(users: {city: params[:city]})
+    all_dresses = Dress.joins(:user).where(users: { city: params[:city] })
     @dresses = all_dresses.select{ |dress| dress.available }
   end
+
+  def show
+    @dress = Dress.find(params[:id])
+  end
+
 end
