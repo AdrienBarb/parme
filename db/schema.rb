e5dba54aebe27656a257af47fa4102fb06e8b289
Romain <romainmostras@icloud.com>
2020-06-09 14:52:58 +0200

    Adding awesome print to Gem File

--- db/schema.rb
+++ db/schema.rb
@@ -53,6 +53,12 @@ ActiveRecord::Schema.define(version: 2020_06_09_102732) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "address"
+    t.string "zip_code"
+    t.string "city"
+    t.string "phone_number"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
