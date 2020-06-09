d5859d565939a4fa14e42e1fcdd239cfde21467f
Romain <romainmostras@icloud.com>
2020-06-09 12:37:10 +0200

    create migration to add user ref to review table

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :dress
+  belongs_to :user
 
   validates :rating, presence: true, inclusion: { in: [1, 2, 3, 4, 5] }
   validates :description, presence: true
