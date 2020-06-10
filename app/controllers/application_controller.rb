c6df6e474d9ffd9c493677b966dc9769a5a84baa
Adrien Barbier <adrien-barbier@hotmail.fr>
2020-06-10 18:06:37 +0200

    correct bug signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   include Pundit
 
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -9,4 +12,9 @@ class ApplicationController < ActionController::Base
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :address, :zip_code, :city, :phone_number, :password)}
+  end
 end
