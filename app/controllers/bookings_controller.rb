522d7701440acf6ea68ca8d4db270caae31626d5
Adrien Barbier <adrien-barbier@hotmail.fr>
2020-06-10 13:45:27 +0200

    Adding view for bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,29 @@
 class BookingsController < ApplicationController
+
+  def new
+    @dress = Dress.find(params[:dress_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @dress = Dress.find(params[:dress_id])
+    @booking = Booking.new(booking_params)
+    @booking.dress = @dress
+    @booking.user = current_user
+    if @booking.save!
+      redirect_to dress_path(@dress)
+    else
+      render :new
+    end
+
+  end
+
+
+private
+
+  def booking_params
+    params.require(:booking).permit(:start_rent_at, :end_rent_at)
+  end
+
+
 end
